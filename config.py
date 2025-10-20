1f63f84eb095358523f2d8c55e27ea6d4b227ccb
ritesh123-long <riteshpadme6@gmail.com>
2025-10-20 12:10:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # placeholder for your Telegram token
-TOKEN = "YOUR_BOT_TOKEN_HERE"
+TOKEN = "8071518517:AAF3huWVr6uAUXtEN3eW6zvs24C07SnZiKk"
